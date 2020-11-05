d16ecace1c610b8aa32a1623b076a1164adcb433
Chahinez Benallal <44991588+ChahiBen@users.noreply.github.com>
2020-11-05 17:01:52 +0100

    change

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -1,35 +1,19 @@
-// src/Controller/BlogController.php
-
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    public function index()
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
     {
-        return new Response('<h1>Page d\'accueil</h1>');
-    }
-
-    public function add()
-    {
-    	return new Response('<h1>Ajouter un article</h1>');
-    }
-
-    public function show($url)
-    {
-    	return new Response('<h1>Lire l\'article ' .$url. '</h1>');
-    }
-
-    public function edit($id)
-    {
-    	return new Response('<h1>Modifier l\'article ' .$id. '</h1>');
-    }
-
-    public function remove($id)
-    {
-    	return new Response('<h1>Supprimer l\'article ' .$id. '</h1>');
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
     }
 }
