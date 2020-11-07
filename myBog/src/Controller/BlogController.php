19e48f8e118a9c09ed39e05dbb4ad47d377f1599
Chahinez Benallal <44991588+ChahiBen@users.noreply.github.com>
2020-11-07 19:50:27 +0100

    clean code

--- myBog/src/Controller/BlogController.php
+++ myBog/src/Controller/BlogController.php
@@ -6,18 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
-    {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
+    public function index(ArticleRepository $repo) {
         $articles = $repo->findAll();
-        //$articles = [new Article(), new Article()];
-
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
             'articles' => $articles
@@ -37,10 +34,7 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id){
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-        $article = $repo->find($id);
-
+    public function show(Article $article){
         return $this->render('blog/show.html.twig',[
             'article' => $article
         ]);
