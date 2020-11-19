74b79240f7fb78bcbf1e855af86acb3f13f88538
Chahinez Benallal <44991588+ChahiBen@users.noreply.github.com>
2020-11-19 12:46:23 +0100

    Adding category and comments

--- myBog/src/Controller/BlogController.php
+++ myBog/src/Controller/BlogController.php
@@ -13,23 +13,13 @@ use Symfony\Component\HttpFoundation\Request;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="blog")
      */
     public function index(ArticleRepository $repo) {
         $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
             'articles' => $articles
-        ]);
-    }
-
-    /**
-     * @Route("/", name="home")
-     */
-    public function home() {
-        return $this->render('blog/home.html.twig', [
-            'title' => 'Bienvenue !',
-            'age' => 24 
         ]);
     }
     
