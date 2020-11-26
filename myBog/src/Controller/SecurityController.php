eae85273b274a28a7e2a9092528f3d0cfb64a408
Chahinez Benallal <44991588+ChahiBen@users.noreply.github.com>
2020-11-26 10:28:18 +0100

    LoginForm

--- myBog/src/Controller/SecurityController.php
+++ myBog/src/Controller/SecurityController.php
@@ -31,6 +31,8 @@ class SecurityController extends AbstractController
 
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('security_login');
         }
 
         return $this->render('security/registration.html.twig',[
@@ -38,5 +40,18 @@ class SecurityController extends AbstractController
 
         ]);
 
+    }
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(){
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout(){
+        
     }
 }
